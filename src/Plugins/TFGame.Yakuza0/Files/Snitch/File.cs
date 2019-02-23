9f1036e47aafc5d46ee1f1501089a4e97aec724b
kaplas <kaplas80@gmail.com>
2019-02-23 20:06:16 +0100

    Fichero snitch.bin del Yakuza 0

---
+++ src/Plugins/TFGame.Yakuza0/Files/Snitch/File.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TF.IO;
+using YakuzaCommon.Files.SimpleSubtitle;
+
+namespace TFGame.Yakuza0.Files.Snitch
+{
+    public class File : YakuzaCommon.Files.SimpleSubtitle.File
+    {
+        public File(string path, string changesFolder, Encoding encoding) : base(path, changesFolder, encoding)
+        {
+        }
+
+        protected override IList<Subtitle> GetSubtitles()
+        {
+            if (HasChanges)
+            {
+                return LoadChanges(ChangesFile);
+            }
+
+            var result = new List<Subtitle>();
+
+            using (var fs = new FileStream(Path, FileMode.Open))
+            using (var input = new ExtendedBinaryReader(fs, FileEncoding, Endianness.BigEndian))
+            {
+                input.Skip(0x08);
+                var count1 = input.ReadInt32();
+                var start1 = input.ReadInt32();
+                var count2 = input.ReadInt32();
+                var start2 = input.ReadInt32();
+
+                for (var i = 0; i < count1 + count2; i++)
+                {
+                    input.Skip(0x8);
+                    var offset = input.ReadInt32();
+                    if (offset > 0)
+                    {
+                        result.Add(ReadSubtitle(input, offset));
+                    }
+                    input.Skip(0x4);
+                }
+            }
+
+            return result;
+        }
+
+        private Subtitle ReadSubtitle(ExtendedBinaryReader input, int offset)
+        {
+            var result = new Subtitle { Offset = offset };
+            var pos = input.Position;
+            input.Seek(offset, SeekOrigin.Begin);
+            result.Text = input.ReadString();
+            result.Loaded = result.Text;
+            result.Translation = result.Text;
+            result.PropertyChanged += SubtitlePropertyChanged;
+            input.Seek(pos, SeekOrigin.Begin);
+
+            return result;
+        }
+
+        public override void Rebuild(string outputFolder)
+        {
+            var outputPath = System.IO.Path.Combine(outputFolder, RelativePath);
+            Directory.CreateDirectory(System.IO.Path.GetDirectoryName(outputPath));
+
+            var subtitles = GetSubtitles();
+
+            using (var fsInput = new FileStream(Path, FileMode.Open))
+            using (var input = new ExtendedBinaryReader(fsInput, FileEncoding, Endianness.BigEndian))
+            using (var fsOutput = new FileStream(outputPath, FileMode.Create))
+            using (var output = new ExtendedBinaryWriter(fsOutput, FileEncoding, Endianness.BigEndian))
+            {
+                output.Write(input.ReadBytes(0x08));
+
+                var count1 = input.ReadInt32();
+                var start1 = input.ReadInt32();
+                var count2 = input.ReadInt32();
+                var start2 = input.ReadInt32();
+
+                output.Write(count1);
+                output.Write(start1);
+                output.Write(count2);
+                output.Write(start2);
+
+                var outputOffset = 0x18 + 0x10 * (count1 + count2);
+
+                var inputStringOffsets = new int[count1 + count2];
+                var inputUnknownOffsets = new int[count1 + count2];
+                var inputUnknownSizes = new int[count1 + count2];
+                for (var i = 0; i < count1 + count2; i++)
+                {
+                    input.Seek(0x18 + 0x10 * i, SeekOrigin.Begin);
+                    input.Skip(8);
+                    inputStringOffsets[i] = input.ReadInt32();
+                    inputUnknownOffsets[i] = input.ReadInt32();
+                }
+
+                for (var i = 0; i < count1 + count2 - 1; i++)
+                {
+                    inputUnknownSizes[i] = (inputStringOffsets[i + 1] - inputUnknownOffsets[i]);
+                }
+
+                inputUnknownSizes[count1 + count2 - 1] = (int) (input.Length - inputUnknownOffsets[count1 + count2 - 1]);
+
+                for (var i = 0; i < count1 + count2; i++)
+                {
+                    input.Seek(0x18 + 0x10 * i, SeekOrigin.Begin);
+                    output.Seek(0x18 + 0x10 * i, SeekOrigin.Begin);
+
+                    output.Write(input.ReadBytes(8));
+
+                    var inputOffset = input.ReadInt32();
+                    outputOffset = WriteString(output, subtitles, inputOffset, outputOffset);
+                    inputOffset = input.ReadInt32();
+                    output.Write(outputOffset);
+                    
+                    input.Seek(inputOffset, SeekOrigin.Begin);
+                    output.Seek(outputOffset, SeekOrigin.Begin);
+                    output.Write(input.ReadBytes(inputUnknownSizes[i]));
+                    outputOffset += inputUnknownSizes[i];
+                }
+            }
+        }
+
+        private int WriteString(ExtendedBinaryWriter output, IList<Subtitle> subtitles, int inputOffset, int outputOffset)
+        {
+            var result = outputOffset;
+
+            if (inputOffset == 0)
+            {
+                output.Write(0);
+            }
+            else
+            {
+                var str = subtitles.First(x => x.Offset == inputOffset);
+                output.Write(outputOffset);
+                var retPos = output.Position;
+                output.Seek(outputOffset, SeekOrigin.Begin);
+                output.WriteString(str.Translation);
+                result = (int)output.Position;
+                output.Seek(retPos, SeekOrigin.Begin);
+            }
+
+            return result;
+        }
+    }
+}
--- src/Plugins/TFGame.Yakuza0/Game.cs
+++ src/Plugins/TFGame.Yakuza0/Game.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using System.Text;
 using TF.Core.Entities;
+using TF.Core.Files.DDS;
 
 namespace TFGame.Yakuza0
 {
@@ -14,10 +15,44 @@ namespace TFGame.Yakuza0
         public override int Version => 1;
         public override Encoding FileEncoding => new YakuzaEncoding();
 
-        public override GameFileContainer[] GetContainers(string path)
+        private IList<GameFileContainer> GetAuth(string path)
         {
-            var result = new List<GameFileContainer>();
+            var cmnSearch =
+                new GameFileSearch
+                {
+                    RelativePath = ".",
+                    SearchPattern = "cmn.bin",
+                    IsWildcard = true,
+                    RecursiveSearch = true,
+                    FileType = typeof(YakuzaCommon.Files.CmnBin.File)
+                };
 
+            var ddsSearch =
+                new GameFileSearch
+                {
+                    RelativePath = ".",
+                    SearchPattern = "*.dds",
+                    IsWildcard = true,
+                    RecursiveSearch = true,
+                    FileType = typeof(DDSFile)
+                };
+
+            var auth_w64_containers = new GameFileContainerSearch
+            {
+                RelativePath = @"data\auth_w64_e",
+                TypeSearch = ContainerType.CompressedFile,
+                RecursiveSearch = false,
+                SearchPattern = "*.par"
+            };
+            auth_w64_containers.Exclusions.Add("inst_auth.par");
+            auth_w64_containers.FileSearches.Add(cmnSearch);
+            auth_w64_containers.FileSearches.Add(ddsSearch);
+
+            return auth_w64_containers.GetContainers(path);
+        }
+
+        private GameFileContainer GetBootpar()
+        {
             var empbSearch =
                 new GameFileSearch
                 {
@@ -46,7 +81,199 @@ namespace TFGame.Yakuza0
             bootpar.FileSearches.Add(empbSearch);
             bootpar.FileSearches.Add(mailSearch);
 
-            result.Add(bootpar);
+            return bootpar;
+        }
+
+        private IList<GameFileContainer> GetMappar(string path)
+        {
+            var imbSearch =
+                new GameFileSearch
+                {
+                    RelativePath = ".",
+                    SearchPattern = "*.imb",
+                    IsWildcard = true,
+                    RecursiveSearch = true,
+                    FileType = typeof(YakuzaCommon.Files.Imb.File)
+                };
+
+            var map_par_containers = new GameFileContainerSearch
+            {
+                RelativePath = @"data\map_par",
+                TypeSearch = ContainerType.CompressedFile,
+                RecursiveSearch = false,
+                SearchPattern = "st_kamuro.par_c;st_kawara_street.par_c"
+            };
+            map_par_containers.FileSearches.Add(imbSearch);
+
+            return map_par_containers.GetContainers(path);
+        }
+
+        private GameFileContainer GetSoundpar()
+        {
+            var mfpSearch =
+                new GameFileSearch
+                {
+                    RelativePath = ".",
+                    SearchPattern = "*.mfp",
+                    IsWildcard = true,
+                    RecursiveSearch = true,
+                    FileType = typeof(YakuzaCommon.Files.Mfpb.File)
+                };
+
+            var soundpar = new GameFileContainer
+            {
+                Path = @"data\soundpar\sound.par",
+                Type = ContainerType.CompressedFile
+            };
+            soundpar.FileSearches.Add(mfpSearch);
+
+            return soundpar;
+        }
+
+        private GameFileContainer GetWdrCommon()
+        {
+            var aiPopupSearch =
+                new GameFileSearch
+                {
+                    RelativePath = ".",
+                    SearchPattern = "ai_popup.bin",
+                    IsWildcard = false,
+                    RecursiveSearch = false,
+                    FileType = typeof(YakuzaCommon.Files.AiPopup.File)
+                };
+
+            var common_armsRepairSearch =
+                new GameFileSearch
+                {
+                    RelativePath = "shop",
+                    SearchPattern = "arms_repair.bin",
+                    IsWildcard = false,
+                    RecursiveSearch = false,
+                    FileType = typeof(YakuzaCommon.Files.SimpleSubtitle.File)
+                };
+
+            var common_blacksmithSearch =
+                new GameFileSearch
+                {
+                    RelativePath = "shop",
+                    SearchPattern = "blacksmith.bin",
+                    IsWildcard = false,
+                    RecursiveSearch = false,
+                    FileType = typeof(YakuzaCommon.Files.Blacksmith.File)
+                };
+
+            var common_presentSearch =
+                new GameFileSearch
+                {
+                    RelativePath = "shop",
+                    SearchPattern = "present.bin;send.bin;throw.bin",
+                    IsWildcard = true,
+                    RecursiveSearch = false,
+                    FileType = typeof(YakuzaCommon.Files.PresentSendThrow.File)
+                };
+
+            var common_saleSearch =
+                new GameFileSearch
+                {
+                    RelativePath = "shop",
+                    SearchPattern = "sale????.bin",
+                    IsWildcard = true,
+                    RecursiveSearch = false,
+                    FileType = typeof(YakuzaCommon.Files.Sale.File)
+                };
+
+            var wdr_par_c_common = new GameFileContainer
+            {
+                Path = @"data\wdr_par_c\common.par",
+                Type = ContainerType.CompressedFile
+            };
+            wdr_par_c_common.FileSearches.Add(aiPopupSearch);
+            wdr_par_c_common.FileSearches.Add(common_armsRepairSearch);
+            wdr_par_c_common.FileSearches.Add(common_blacksmithSearch);
+            wdr_par_c_common.FileSearches.Add(common_presentSearch);
+            wdr_par_c_common.FileSearches.Add(common_saleSearch);
+
+            return wdr_par_c_common;
+        }
+
+        private GameFileContainer GetWdr()
+        {
+            var wdr_barSearch =
+                new GameFileSearch
+                {
+                    RelativePath = "shop",
+                    SearchPattern = "bar????.bin",
+                    IsWildcard = true,
+                    RecursiveSearch = false,
+                    FileType = typeof(YakuzaCommon.Files.Bar.File)
+                };
+
+            var wdr_restaurantSearch =
+                new GameFileSearch
+                {
+                    RelativePath = "shop",
+                    SearchPattern = "restaurant????.bin;ex_shop????.bin;shop????.bin",
+                    IsWildcard = true,
+                    RecursiveSearch = false,
+                    FileType = typeof(YakuzaCommon.Files.Restaurant.File)
+                };
+
+            var snitchSearch =
+                new GameFileSearch
+                {
+                    RelativePath = ".",
+                    SearchPattern = "snitch.bin",
+                    IsWildcard = false,
+                    RecursiveSearch = false,
+                    FileType = typeof(Files.Snitch.File)
+                };
+
+            var wdr_par = new GameFileContainer
+            {
+                Path = @"data\wdr_par_c\wdr.par",
+                Type = ContainerType.CompressedFile
+            };
+
+            wdr_par.FileSearches.Add(wdr_barSearch);
+            wdr_par.FileSearches.Add(wdr_restaurantSearch);
+            wdr_par.FileSearches.Add(snitchSearch);
+
+            return wdr_par;
+        }
+
+        private GameFileContainer GetReactorpar()
+        {
+            var nameSearch =
+                new GameFileSearch
+                {
+                    RelativePath = ".",
+                    SearchPattern = "name_?.bin",
+                    IsWildcard = true,
+                    RecursiveSearch = false,
+                    FileType = typeof(YakuzaCommon.Files.Name.File)
+                };
+
+            var reactor = new GameFileContainer
+            {
+                Path = @"data\reactorpar\reactor_w64.par",
+                Type = ContainerType.CompressedFile
+            };
+
+            reactor.FileSearches.Add(nameSearch);
+            return reactor;
+        }
+
+        public override GameFileContainer[] GetContainers(string path)
+        {
+            var result = new List<GameFileContainer>();
+
+            //result.AddRange(GetAuth(path));
+            //result.Add(GetBootpar());
+            //result.AddRange(GetMappar(path));
+            //result.Add(GetReactorpar());
+            //result.Add(GetSoundpar());
+            //result.Add(GetWdrCommon());
+            result.Add(GetWdr());
 
             result.Sort();
             return result.ToArray();
