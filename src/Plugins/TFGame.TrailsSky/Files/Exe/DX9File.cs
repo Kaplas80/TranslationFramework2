a23413e0401b44346b88f3865439d046feb9225c
kaplas <kaplas80@gmail.com>
2019-04-01 17:42:36 +0200

    Corregido el problema con las celdas numéricas

---
+++ src/Plugins/TFGame.TrailsSky/Files/Exe/DX9File.cs
@@ -0,0 +1,11 @@
+﻿namespace TFGame.TrailsSky.Files.Exe
+{
+    public class DX9File : File
+    {
+        protected override long FontTableOffset => 0x117DA8;
+
+        public DX9File(string path, string changesFolder) : base(path, changesFolder)
+        {
+        }
+    }
+}
