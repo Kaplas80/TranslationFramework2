08fb21d62834939ccf85755f179fc833ad19d15e
kaplas <kaplas80@gmail.com>
2019-04-01 12:43:47 +0200

    Arreglado bug en la generación del snitch.bin de Yakuza 0

--- src/Plugins/TFGame.Yakuza0/Files/Snitch/File.cs
+++ src/Plugins/TFGame.Yakuza0/Files/Snitch/File.cs
@@ -96,8 +96,9 @@ namespace TFGame.Yakuza0.Files.Snitch
                     var inputSubtitle = ReadSubtitle(input);
                     outputOffset = WriteSubtitle(output, subtitles, inputSubtitle.Offset, outputOffset);
 
+                    output.Skip(4);
                     var inputOffset = input.ReadInt32();
-                    output.Write(outputOffset);
+                    output.Write((int)outputOffset);
                     
                     input.Seek(inputOffset, SeekOrigin.Begin);
                     output.Seek(outputOffset, SeekOrigin.Begin);
