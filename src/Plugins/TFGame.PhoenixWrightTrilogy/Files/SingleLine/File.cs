8403ec362f469ca1af05f4a0303d536173104113
kaplas <kaplas80@gmail.com>
2019-04-13 11:02:23 +0200

    Version funcional en Grid

---
+++ src/Plugins/TFGame.PhoenixWrightTrilogy/Files/SingleLine/File.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TF.Core.TranslationEntities;
+using TF.IO;
+
+namespace TFGame.PhoenixWrightTrilogy.Files.SingleLine
+{
+    public class File : EncryptedFile
+    {
+        public File(string path, string changesFolder, System.Text.Encoding encoding) : base(path, changesFolder, encoding)
+        {
+        }
+
+        protected override IList<Subtitle> GetSubtitles()
+        {
+            var result = new List<Subtitle>();
+            var encryptedData = System.IO.File.ReadAllBytes(Path);
+            var data = DecryptData(encryptedData);
+
+            using (var ms = new MemoryStream(data))
+            using (var input = new ExtendedBinaryReader(ms, FileEncoding))
+            {
+                while (input.Position + 2 <= input.Length)
+                {
+                    var offset = input.Position;
+                    var id = input.ReadUInt16();
+                    var text = input.ReadString();
+                    text = ToHalfWidthChars(text);
+                    var subtitle = new Subtitle {Offset = offset, Text = text, Translation = text, Loaded = text};
+                    subtitle.PropertyChanged += SubtitlePropertyChanged;
+                    result.Add(subtitle);
+                }
+            }
+
+            LoadChanges(result);
+
+            return result;
+        }
+
+        public override void Rebuild(string outputFolder)
+        {
+            var outputPath = System.IO.Path.Combine(outputFolder, RelativePath);
+            Directory.CreateDirectory(System.IO.Path.GetDirectoryName(outputPath));
+
+            var subtitles = GetSubtitles();
+
+            var encryptedInputData = System.IO.File.ReadAllBytes(Path);
+            var inputData = DecryptData(encryptedInputData);
+            byte[] outputData;
+
+            using (var msInput = new MemoryStream(inputData))
+            using (var input = new ExtendedBinaryReader(msInput, FileEncoding))
+            using (var msOutput = new MemoryStream())
+            using (var output = new ExtendedBinaryWriter(msOutput, FileEncoding))
+            {
+                while (input.Position + 2 <= input.Length)
+                {
+                    var offset = input.Position;
+                    var id = input.ReadUInt16();
+                    var inputText = input.ReadString();
+
+                    var subtitle = subtitles.First(x => x.Offset == offset);
+                    var outputText = ToFullWidthChars(subtitle.Translation);
+                    output.Write(id);
+                    output.WriteString(outputText);
+                }
+
+                outputData = msOutput.ToArray();
+            }
+
+            var encryptedOutputData = EncryptData(outputData);
+            System.IO.File.WriteAllBytes(outputPath, encryptedOutputData);
+        }
+    }
+}
---
+++ src/Plugins/TFGame.PhoenixWrightTrilogy/Game.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using TF.Core.Entities;
+
+namespace TFGame.PhoenixWrightTrilogy
+{
+    public class Game : IGame
+    {
+        public string Id => "762580b0-6bf3-49df-8d09-d6b8804b79c6";
+        public string Name => "Phoenix Wright: Ace Attorney Trilogy";
+        public string Description => "Build Id 3633830";
+        public Image Icon => Resources.Icon; // https://www.deviantart.com/dovellas/art/Phoenix-Wright-56731742
+        public int Version => 1;
+        public System.Text.Encoding FileEncoding => new Encoding();
+
+        private GameFileContainer GetScenario(string scenario)
+        {
+            var scenarioSingleLineSearch = new GameFileSearch()
+            {
+                RelativePath = @".",
+                SearchPattern = $"{scenario}_u.cho;{scenario}_sel_u.bin;{scenario}_ba_u.bin;{scenario}_nolb_u.bin;",
+                IsWildcard = true,
+                RecursiveSearch = false,
+                FileType = typeof(Files.SingleLine.File)
+            };
+
+            var scenarioMultiLineSearch = new GameFileSearch()
+            {
+                RelativePath = @".",
+                SearchPattern = $"{scenario}_note_u.bin;",
+                IsWildcard = true,
+                RecursiveSearch = false,
+                FileType = typeof(Files.MultiLine.File)
+            };
+
+            var scenarioMdtSearch = new GameFileSearch()
+            {
+                RelativePath = @".",
+                SearchPattern = $"*_u.mdt;",
+                IsWildcard = true,
+                RecursiveSearch = false,
+                FileType = typeof(Files.Mdt.File)
+            };
+
+            var container = new GameFileContainer
+            {
+                Path = $@"PWAAT_Data\StreamingAssets\{scenario.ToUpper()}\scenario",
+                Type = ContainerType.Folder
+            };
+            container.FileSearches.Add(scenarioSingleLineSearch);
+            container.FileSearches.Add(scenarioMultiLineSearch);
+            container.FileSearches.Add(scenarioMdtSearch);
+
+            return container;
+        }
+
+        public GameFileContainer[] GetContainers(string path)
+        {
+            var result = new List<GameFileContainer>();
+
+            var menuMultiLineSearch = new GameFileSearch()
+            {
+                RelativePath = @".",
+                SearchPattern = "title_text_u.bin;option_text_u.bin;save_text_u.bin;system_text_u.bin;credit_text.bin",
+                IsWildcard = true,
+                RecursiveSearch = false,
+                FileType = typeof(Files.MultiLine.File)
+            };
+
+            var menuSingleLineSearch = new GameFileSearch()
+            {
+                RelativePath = @".",
+                SearchPattern = "common_text_u.bin;platform_text_u.bin",
+                IsWildcard = true,
+                RecursiveSearch = false,
+                FileType = typeof(Files.SingleLine.File)
+            };
+
+            var menu = new GameFileContainer
+            {
+                Path = @"PWAAT_Data\StreamingAssets\menu\text",
+                Type = ContainerType.Folder
+            };
+            menu.FileSearches.Add(menuMultiLineSearch);
+            menu.FileSearches.Add(menuSingleLineSearch);
+
+            result.Add(menu);
+
+            result.Add(GetScenario("gs1"));
+            result.Add(GetScenario("gs2"));
+            result.Add(GetScenario("gs3"));
+
+            return result.ToArray();
+        }
+
+        public void ExtractFile(string inputFile, string outputPath)
+        {
+        }
+
+        public void RepackFile(string inputPath, string outputFile, bool compress)
+        {
+        }
+    }
+}
+
