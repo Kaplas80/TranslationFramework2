bde8927dd01c7dbf5e0554b6463d7ce37c6316bc
kaplas <kaplas80@gmail.com>
2019-04-16 15:09:10 +0200

    Soporte para los textos en el Exe del Trails in the Sky

--- src/Plugins/TFGame.TrailsSky/Files/Exe/DX9File.cs
+++ src/Plugins/TFGame.TrailsSky/Files/Exe/DX9File.cs
@@ -1,10 +1,19 @@
-﻿namespace TFGame.TrailsSky.Files.Exe
+﻿using System;
+using System.Collections.Generic;
+
+namespace TFGame.TrailsSky.Files.Exe
 {
     public class DX9File : File
     {
         protected override long FontTableOffset => 0x117DA8;
 
-        public DX9File(string path, string changesFolder) : base(path, changesFolder)
+        protected override List<Tuple<long, long>> AllowedStringOffsets => new List<Tuple<long, long>>()
+        {
+            new Tuple<long, long>(0x117754, 0x11C394),
+            new Tuple<long, long>(0x11EDA8, 0x11F168),
+        };
+
+        public DX9File(string path, string changesFolder, Encoding encoding) : base(path, changesFolder, encoding)
         {
         }
     }
--- src/TF.Core/Files/TextFile.cs
+++ src/TF.Core/Files/TextFile.cs
@@ -103,7 +103,7 @@ namespace TF.Core.Files
 
                     if (version != ChangesFileVersion)
                     {
-                        File.Delete(ChangesFile);
+                        //File.Delete(ChangesFile);
                         return;
                     }
 
