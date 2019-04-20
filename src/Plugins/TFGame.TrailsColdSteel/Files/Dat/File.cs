bce5588aa86abff66af478ca9bd8460d0a2492e0
kaplas <kaplas80@gmail.com>
2019-04-20 21:17:02 +0200

    Primera carga de los ficheros .dat Todavía no hace nada útil

--- src/Plugins/TFGame.TrailsColdSteel/Files/Dat/File.cs
+++ src/Plugins/TFGame.TrailsColdSteel/Files/Dat/File.cs
@@ -10,25 +10,121 @@ using TF.IO;
 
 namespace TFGame.TrailsColdSteel.Files.Dat
 {
-    public class File : BinaryTextFile
+    public class File : TextFile
     {
-        public File(string path, string changesFolder, Encoding encoding) : base(path, changesFolder, encoding)
+        private class Op
         {
+            public byte Code;
+            public string Name;
+            public int ParamCount;
         }
 
-        protected override IList<TF.Core.TranslationEntities.Subtitle> GetSubtitles()
+        private readonly Op[] GameOps;
+
+        public File(string path, string changesFolder, Encoding encoding) : base(path, changesFolder, encoding)
         {
-            var result = new List<Subtitle>();
+            GameOps = new Op[160];
+
+            GameOps[0x00] = new Op { Code = 0x00, Name = "Nop", ParamCount = 0 };
+            GameOps[0x01] = new Op {Code = 0x01, Name = "Return", ParamCount = 0};
+        }
+
+        protected override PlainText GetText()
+        {
+            var result = new PlainText();
+            var txt = new StringBuilder();
 
             using (var fs = new FileStream(Path, FileMode.Open))
             using (var input = new ExtendedBinaryReader(fs, FileEncoding))
             {
-                input.Seek(0x18, SeekOrigin.Begin);
-                var startOffset = input.ReadInt32();
-                input.Seek(startOffset, SeekOrigin.Begin);
+                // Header
+                var headerSize = input.ReadUInt32(); // 0x20
+                var scnNamePointer = input.ReadUInt32(); // 0x20
+                var functionOffsetTablePointer = input.ReadUInt32();
+                var functionOffsetTableSize = input.ReadUInt32();
+                var functionNameOffsetTablePointer = input.ReadUInt32();
+                var functionCount = input.ReadUInt32();
+                var dataStartPointer = input.ReadUInt32();
+                var dummy = input.ReadUInt32(); // 0xABCDEF00
 
+                input.Seek(scnNamePointer, SeekOrigin.Begin);
+                var scnName = input.ReadString(Encoding.ASCII);
 
+                var functionOffset = new uint[functionCount];
+                var functionSize = new uint[functionCount];
+
+                var functionNameOffset = new ushort[functionCount];
+                var functionName = new string[functionCount];
+
+                input.Seek(functionOffsetTablePointer, SeekOrigin.Begin);
+                for (var i = 0; i < functionCount; i++)
+                {
+                    functionOffset[i] = input.ReadUInt32();
+                }
+
+                for (var i = 0; i < functionCount - 1; i++)
+                {
+                    functionSize[i] = functionOffset[i + 1] - functionOffset[i];
+                }
+                functionSize[functionCount - 1] = (uint) (fs.Length - functionOffset[functionCount - 1]);
+
+                input.Seek(functionNameOffsetTablePointer, SeekOrigin.Begin);
+                for (var i = 0; i < functionCount; i++)
+                {
+                    functionNameOffset[i] = input.ReadUInt16();
+                }
+
+                for (var i = 0; i < functionCount; i++)
+                {
+                    input.Seek(functionNameOffset[i], SeekOrigin.Begin);
+                    functionName[i] = input.ReadString(Encoding.ASCII);
+                }
+
+                input.Seek(dataStartPointer, SeekOrigin.Begin);
+                input.SkipPadding(0x04);
+
+                for (var i = 0; i < functionCount; i++)
+                {
+                    input.Seek(functionOffset[i], SeekOrigin.Begin);
+                    if (i > 0)
+                    {
+                        txt.AppendLine();
+                    }
+
+                    var name = string.IsNullOrEmpty(functionName[i]) ? "AnonymousFunc" : functionName[i];
+                    txt.AppendLine($"{name}({functionOffset[i]:X8})");
+
+                    var count = 0;
+                    while (count < functionSize[i])
+                    {
+                        var opCode = input.ReadByte();
+                        count++;
+
+                        if (opCode < 160 && GameOps[opCode] != null)
+                        {
+                            var op = GameOps[opCode];
+
+                            var args = new string[op.ParamCount];
+                            for (var j = 0; j < op.ParamCount; j++)
+                            {
+                                args[j] = string.Concat(input.ReadByte().ToString());
+                            }
+
+                            txt.AppendLine($"{op.Name}({string.Join(", ", args)});");
+                        }
+                        else
+                        {
+                            txt.AppendLine($"Op_{opCode:X2}();");
+                        }
+
+                    }
+                }
             }
+
+            result.Text = txt.ToString();
+            result.Translation = txt.ToString();
+            result.Loaded = txt.ToString();
+            result.PropertyChanged += SubtitlePropertyChanged;
 
             LoadChanges(result);
 
--- src/Plugins/TFGame.TrailsColdSteel/Game.cs
+++ src/Plugins/TFGame.TrailsColdSteel/Game.cs
@@ -4,15 +4,14 @@ using System.Drawing;
 using System.IO;
 using System.Text;
 using TF.Core.Entities;
-using TFGame.TrailsColdSteel;
 
-namespace TFGame.TrailsSky
+namespace TFGame.TrailsColdSteel
 {
     public class Game : IGame
     {
-        public string Id => "7d38ad5e-2f86-4e8d-85c2-87571a7b333e";
+        public string Id => "3e8301f7-0417-4707-8db9-ee856cea5761";
         public string Name => "The Legend of Heroes: Trails of Cold Steel";
-        public string Description => "Versión en inglés Steam v1.6";
+        public string Description => "Build Id: 3170008";
         public Image Icon => Resources.Icon; // https://www.deviantart.com/andonovmarko/art/The-Legend-of-Heroes-Trails-of-Cold-Steel-Icon-704948381
         public int Version => 1;
         public System.Text.Encoding FileEncoding => Encoding.UTF8;
@@ -21,314 +20,25 @@ namespace TFGame.TrailsSky
         {
             var result = new List<GameFileContainer>();
 
-            /*var exe = new GameFileSearch()
+            var scenarioSearch = new GameFileSearch
             {
                 RelativePath = ".",
-                SearchPattern = "ed6_win.exe",
-                IsWildcard = false,
+                SearchPattern = "*.dat",
+                IsWildcard = true,
                 RecursiveSearch = false,
-                FileType = typeof(Files.Exe.File)
+                FileType = typeof(Files.Dat.File)
             };
 
-            var exe2 = new GameFileSearch()
+            var scenarioContainer = new GameFileContainer
             {
-                RelativePath = ".",
-                SearchPattern = "ed6_win_DX9.exe",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.Exe.DX9File)
-            };
-
-            var root = new GameFileContainer
-            {
-                Path = @".\",
+                Path = @"data\scripts\scena\dat_us",
                 Type = ContainerType.Folder
             };
-            root.FileSearches.Add(exe);
-            root.FileSearches.Add(exe2);
 
-            var fonts = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "FONT*._DA",
-                IsWildcard = true,
-                RecursiveSearch = false,
-                FileType = typeof(Files.FONT.File)
-            };
+            scenarioContainer.FileSearches.Add(scenarioSearch);
 
-            var imagesT1 = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "C_BTN01 ._CH;C_BTN02 ._CH;C_EMOTIO._CH;C_ICON1 ._CH;C_MOUSE ._CH",
-                IsWildcard = true,
-                RecursiveSearch = false,
-                FileType = typeof(Files.Images.ImageType1)
-            };
+            result.Add(scenarioContainer);
 
-            var imagesT2 = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "H_BTN01 ._CH;H_BTN02 ._CH;H_EMOTIO._CH;H_ICON1 ._CH;H_MOUSE ._CH",
-                IsWildcard = true,
-                RecursiveSearch = false,
-                FileType = typeof(Files.Images.ImageType2)
-            };
-
-            var imagesT3 = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "C_CAMP02._CH",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.Images.ImageType3)
-            };
-
-            var imagesT4 = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "H_CAMP02._CH",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.Images.ImageType4)
-            };
-
-            var dt00 = new GameFileContainer
-            {
-                Path = @".\ED6_DT00.dat",
-                Type = ContainerType.CompressedFile
-            };
-            dt00.FileSearches.Add(fonts);
-            dt00.FileSearches.Add(imagesT1);
-            dt00.FileSearches.Add(imagesT2);
-            dt00.FileSearches.Add(imagesT3);
-            dt00.FileSearches.Add(imagesT4);
-
-            var scenarios = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "*._SN",
-                IsWildcard = true,
-                RecursiveSearch = false,
-                FileType = typeof(Files.SN.File)
-            };
-
-            var dt01 = new GameFileContainer
-            {
-                Path = @".\ED6_DT01.dat",
-                Type = ContainerType.CompressedFile
-            };
-
-            dt01.FileSearches.Add(scenarios);
-
-            var bookFiles = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "T_BOOK??._DT",
-                IsWildcard = true,
-                RecursiveSearch = false,
-                FileType = typeof(Files.DT.BookFile),
-                Exclusions = { "T_BOOK00._DT" }
-            };
-
-            var cookFile = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "T_COOK2 ._DT",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.DT.CookFile),
-            };
-
-            var itemFile = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "T_ITEM2 ._DT",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.DT.ItemFile),
-            };
-
-            var magicFile = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "T_MAGIC ._DT",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.DT.MagicFile),
-            };
-
-            var memoFile = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "T_MEMO  ._DT",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.DT.MemoFile),
-            };
-
-            var nameFile = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "T_NAME  ._DT",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.DT.NameFile),
-            };
-
-            var questFile = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "T_QUEST ._DT",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.DT.QuestFile),
-            };
-
-            var shopFile = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "T_SHOP  ._DT",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.DT.ShopFile),
-            };
-
-            var townFile = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "T_TOWN  ._DT",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.DT.TownFile),
-            };
-
-            var dt02 = new GameFileContainer
-            {
-                Path = @".\ED6_DT02.dat",
-                Type = ContainerType.CompressedFile
-            };
-
-            dt02.FileSearches.Add(bookFiles);
-            dt02.FileSearches.Add(cookFile);
-            dt02.FileSearches.Add(itemFile);
-            dt02.FileSearches.Add(magicFile);
-            dt02.FileSearches.Add(memoFile);
-            dt02.FileSearches.Add(nameFile);
-            dt02.FileSearches.Add(questFile);
-            dt02.FileSearches.Add(shopFile);
-            dt02.FileSearches.Add(townFile);
-
-            var imagesDT04 = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "C_ENCNT1._CH;C_GAMEOV._CH;C_NOTE??._CH;C_VIS018._CH;C_VIS019._CH;C_VIS020._CH;C_VIS021._CH;C_VIS022._CH;C_VIS023._CH;C_VIS024._CH;C_VIS025._CH;C_VIS026._CH;C_VIS027._CH;C_VIS040._CH;C_VIS041._CH;C_VIS042._CH;C_VIS043._CH;C_VIS044._CH;C_VIS045._CH;C_VIS046._CH;C_VIS047._CH;C_VIS048._CH;C_VIS049._CH;",
-                IsWildcard = true,
-                RecursiveSearch = false,
-                FileType = typeof(Files.Images.ImageType5),
-                Exclusions =
-                {
-                    "C_NOTE01._CH",
-                    "C_NOTE02._CH",
-                    "C_NOTE21._CH",
-                    "C_NOTE22._CH",
-                }
-            };
-
-            var imagesDT04_2 = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "H_ENCNT1._CH;H_GAMEOV._CH;H_NOTE??._CH;H_VIS018._CH;H_VIS019._CH;H_VIS020._CH;H_VIS021._CH;H_VIS022._CH;H_VIS023._CH;H_VIS024._CH;H_VIS025._CH;H_VIS026._CH;H_VIS027._CH;H_VIS040._CH;H_VIS041._CH;H_VIS042._CH;H_VIS043._CH;H_VIS044._CH;H_VIS045._CH;H_VIS046._CH;H_VIS047._CH;H_VIS048._CH;H_VIS049._CH;",
-                IsWildcard = true,
-                RecursiveSearch = false,
-                FileType = typeof(Files.Images.ImageType6),
-                Exclusions =
-                {
-                    "H_NOTE01._CH",
-                    "H_NOTE02._CH",
-                    "H_NOTE21._CH",
-                    "H_NOTE22._CH",
-                }
-            };
-
-            var dt04 = new GameFileContainer
-            {
-                Path = @".\ED6_DT04.dat",
-                Type = ContainerType.CompressedFile
-            };
-            dt04.FileSearches.Add(imagesDT04);
-            dt04.FileSearches.Add(imagesDT04_2);
-
-            var imagesDT0F = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "BATTLE  ._CH;BATTLE2 ._CH;BTLMENU ._CH",
-                IsWildcard = true,
-                RecursiveSearch = false,
-                FileType = typeof(Files.Images.ImageType1)
-            };
-
-            var imagesDT0F_2 = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "HBATTLE ._CH;HBATTLE2._CH;HBTLMENU._CH",
-                IsWildcard = true,
-                RecursiveSearch = false,
-                FileType = typeof(Files.Images.ImageType1)
-            };
-
-            var dt0F = new GameFileContainer
-            {
-                Path = @".\ED6_DT0F.dat",
-                Type = ContainerType.CompressedFile
-            };
-            dt0F.FileSearches.Add(imagesDT0F);
-            dt0F.FileSearches.Add(imagesDT0F_2);
-
-            var imagesDT1C_1 = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "AREA    ._CH",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.Images.ImageType1)
-            };
-
-            var imagesDT1C_2 = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "ICONS   ._CH",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.Images.ImageType1)
-            };
-
-            var imagesDT1C_3 = new GameFileSearch
-            {
-                RelativePath = ".",
-                SearchPattern = "HICONS  ._CH",
-                IsWildcard = false,
-                RecursiveSearch = false,
-                FileType = typeof(Files.Images.ImageType2)
-            };
-
-            var dt1C = new GameFileContainer
-            {
-                Path = @".\ED6_DT1C.dat",
-                Type = ContainerType.CompressedFile
-            };
-
-            dt1C.FileSearches.Add(imagesDT1C_1);
-            dt1C.FileSearches.Add(imagesDT1C_2);
-            dt1C.FileSearches.Add(imagesDT1C_3);
-
-            result.Add(root);
-            result.Add(dt00);
-            result.Add(dt01);
-            result.Add(dt02);
-            result.Add(dt04);
-            result.Add(dt0F);
-            result.Add(dt1C);
-            */
             return result.ToArray();
         }
 
@@ -342,6 +52,14 @@ namespace TFGame.TrailsSky
         {
             var fileName = Path.GetFileName(outputFile);
             var extension = Path.GetExtension(outputFile);
+        }
+
+        public void PreprocessContainer(TranslationFileContainer container, string containerPath, string extractionPath)
+        {
+        }
+
+        public void PostprocessContainer(TranslationFileContainer container, string containerPath, string extractionPath)
+        {
         }
     }
 }
