dd512adbbdf73fa7b8b45e308fe166e29cc58928
kaplas <kaplas80@gmail.com>
2019-05-12 12:04:12 +0200

    Soporte inicial para Yakuza Kiwami 2

---
+++ src/ExternalTools/ParTool/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using YakuzaGame.Files;
+
+namespace ParTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var isFile = File.Exists(args[0]);
+            if (isFile)
+            {
+                ParFile.Extract(args[0], $"{args[0]}.unpack");
+                return;
+            }
+
+            var isDirectory = Directory.Exists(args[0]);
+
+            if (isDirectory)
+            {
+                if (args[0].EndsWith(".unpack"))
+                {
+                    ParFile.Repack($"{args[0]}.unpack", args[0], true);
+                    return;
+                }
+                else
+                {
+                    var parFiles = Directory.GetFiles(args[0], "*.par", SearchOption.AllDirectories);
+
+                    foreach (var file in parFiles)
+                    {
+                        ParFile.Extract(file, $"{file}.unpack");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Plugins/TFGame.YakuzaKiwami2/Game.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using TF.Core.Entities;
+using TF.Core.Files;
+
+namespace TFGame.YakuzaKiwami2
+{
+    public class Game : YakuzaGame.Game
+    {
+
+        public override string Id => "28bb0b85-2455-4cae-bb6a-fcc6b726c823";
+        public override string Name => "Yakuza Kiwami 2";
+        public override string Description => "Build Id: 3762252";
+        public override Image Icon => Resources.Icon; // https://www.deviantart.com/andonovmarko/art/Yakuza-Kiwami-2-Icon-794434106
+        public override int Version => 1;
+        public override System.Text.Encoding FileEncoding => new Encoding();
+
+        private GameFileContainer GetDb()
+        {
+            var search =
+                new GameFileSearch
+                {
+                    RelativePath = @"en",
+                    SearchPattern = "*.bin",
+                    IsWildcard = true,
+                    RecursiveSearch = false,
+                    //FileType = typeof(DDSFile)
+                };
+
+            var folder = new GameFileContainer
+            {
+                Path = @"data\db.par",
+                Type = ContainerType.CompressedFile
+            };
+            folder.FileSearches.Add(search);
+            return folder;
+        }
+
+        private GameFileContainer GetFont()
+        {
+            var ddsSearch =
+                new GameFileSearch
+                {
+                    RelativePath = @"en",
+                    SearchPattern = "*.dds",
+                    IsWildcard = true,
+                    RecursiveSearch = false,
+                    FileType = typeof(DDSFile)
+                };
+
+            var font = new GameFileContainer
+            {
+                Path = @"data\font",
+                Type = ContainerType.Folder
+            };
+            font.FileSearches.Add(ddsSearch);
+            return font;
+        }
+
+        private GameFileContainer GetUi()
+        {
+            var search =
+                new GameFileSearch
+                {
+                    RelativePath = @"en",
+                    SearchPattern = "*.dds",
+                    IsWildcard = true,
+                    RecursiveSearch = true,
+                    FileType = typeof(DDSFile)
+                };
+
+            var folder = new GameFileContainer
+            {
+                Path = @"data\ui.par",
+                Type = ContainerType.CompressedFile
+            };
+            folder.FileSearches.Add(search);
+            return folder;
+        }
+
+        public override GameFileContainer[] GetContainers(string path)
+        {
+            var result = new List<GameFileContainer>();
+
+            result.Add(GetDb());
+            result.Add(GetFont());
+            result.Add(GetUi());
+
+            result.Sort();
+            return result.ToArray();
+        }
+    }
+}
