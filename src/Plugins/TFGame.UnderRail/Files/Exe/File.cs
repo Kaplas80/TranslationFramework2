7adb27abde51fb87bfa83486c7bcfe85596ccaeb
kaplas <kaplas80@gmail.com>
2019-09-05 12:59:14 +0200

    Primera versión. Traducción del EXE

---
+++ src/Plugins/TFGame.UnderRail/Files/Exe/File.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using TF.Core.Files;
+using TF.Core.TranslationEntities;
+
+namespace TFGame.UnderRail.Files.Exe
+{
+    public class File : BinaryTextFile
+    {
+        public File(string path, string changesFolder, System.Text.Encoding encoding) : base(path, changesFolder, encoding)
+        {
+        }
+
+        protected override IList<Subtitle> GetSubtitles()
+        {
+            var result = new List<Subtitle>();
+            var module = ModuleDefMD.Load(Path);
+            var usStream = module.USStream;
+            var reader = usStream.CreateReader();
+
+            reader.Position = 1;
+            while (reader.Position < reader.Length) 
+            {
+                var offset = (uint)reader.Position;
+                if (!reader.TryReadCompressedUInt32(out var len)) 
+                {
+                    if (offset == reader.Position)
+                    {
+                        reader.Position++;
+                    }
+                    continue;
+                }
+
+                if (len == 0 || (ulong) reader.Position + len > reader.Length)
+                {
+                    continue;
+                }
+
+                var stringLen = (int)len / 2;
+                var str = reader.ReadUtf16String(stringLen);
+                if ((len & 1) != 0)
+                {
+                    reader.ReadByte();
+                }
+
+                var subtitle = new Subtitle {Text = str, Loaded = str, Translation = str, Offset = offset};
+                subtitle.PropertyChanged += SubtitlePropertyChanged;
+                result.Add(subtitle);
+            }
+
+            result.Sort();
+            LoadChanges(result);
+
+            return result;
+        }
+
+        public override void Rebuild(string outputFolder)
+        {
+            var outputPath = System.IO.Path.Combine(outputFolder, RelativePath);
+            Directory.CreateDirectory(System.IO.Path.GetDirectoryName(outputPath));
+
+            var subtitles = GetSubtitles();
+
+            var dict = subtitles.Where(subtitle => subtitle.Text != subtitle.Translation).ToDictionary(subtitle => subtitle.Text, subtitle => subtitle.Translation);
+            var module = ModuleDefMD.Load(Path);
+            
+            foreach (var t in module.Types)
+            {
+                foreach (var m in t.Methods)
+                {
+                    if (!m.HasBody)
+                    {
+                        continue;
+                    }
+
+                    foreach (var instr in m.Body.Instructions)
+                    {
+                        if (instr.OpCode != OpCodes.Ldstr)
+                        {
+                            continue;
+                        }
+
+                        var str = (string) instr.Operand;
+
+                        if (dict.TryGetValue(str, out var translation))
+                        {
+                            instr.Operand = translation;
+                        }
+                    }
+                }
+            }
+            
+            module.Write(outputPath);
+        }
+    }
+}
---
+++ src/Plugins/TFGame.UnderRail/Game.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using TF.Core.Entities;
+
+namespace TFGame.UnderRail
+{
+    public class Game : IGame
+    {
+        public string Id => "ad08639d-0f5b-47fb-aa9d-b412e44ecfcd";
+        public string Name => "UnderRail";
+        public string Description => "v1.1.0.12";
+        public Image Icon => Resources.Icon; // https://www.deviantart.com/sony33d/art/UnderRail-584195186
+        public int Version => 2;
+        public System.Text.Encoding FileEncoding => new Encoding();
+
+        public GameFileContainer[] GetContainers(string path)
+        {
+            var result = new List<GameFileContainer>();
+
+            var exe = new GameFileSearch()
+            {
+                RelativePath = ".",
+                SearchPattern = "underrail.exe",
+                IsWildcard = false,
+                RecursiveSearch = false,
+                FileType = typeof(Files.Exe.File)
+            };
+
+            var root = new GameFileContainer
+            {
+                Path = @".\",
+                Type = ContainerType.Folder
+            };
+            root.FileSearches.Add(exe);
+
+            result.Add(root);
+            return result.ToArray();
+        }
+
+        public void ExtractFile(string inputFile, string outputPath)
+        {
+            var fileName = Path.GetFileName(inputFile);
+            var extension = Path.GetExtension(inputFile);
+
+            if (extension.StartsWith(".dat"))
+            {
+            }
+        }
+
+        public void RepackFile(string inputPath, string outputFile, bool compress)
+        {
+            var fileName = Path.GetFileName(outputFile);
+            var extension = Path.GetExtension(outputFile);
+
+            if (extension.StartsWith(".dat"))
+            {
+            }
+        }
+
+        public void PreprocessContainer(TranslationFileContainer container, string containerPath, string extractionPath)
+        {
+        }
+
+        public void PostprocessContainer(TranslationFileContainer container, string containerPath, string extractionPath)
+        {
+        }
+    }
+}
