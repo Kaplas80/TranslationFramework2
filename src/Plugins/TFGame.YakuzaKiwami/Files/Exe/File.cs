53535b665dbc99aee005e6babefc605269b0b1cc
kaplas <kaplas80@gmail.com>
2020-04-21 11:43:36 +0200

    More Kiwami exe missing texts

--- src/Plugins/TFGame.YakuzaKiwami/Files/Exe/File.cs
+++ src/Plugins/TFGame.YakuzaKiwami/Files/Exe/File.cs
@@ -16,9 +16,12 @@ namespace TFGame.YakuzaKiwami.Files.Exe
         {
             new Tuple<long, long>(0x00C93710, 0x00C93710),
             new Tuple<long, long>(0x00C98B78, 0x00C98B90),
+            new Tuple<long, long>(0x00CA1DD8, 0x00CA1DE8),
             new Tuple<long, long>(0x00CA2020, 0x00CA2260),
             new Tuple<long, long>(0x00CA2E00, 0x00CA2E40),
             new Tuple<long, long>(0x00CA37C8, 0x00CA4600),
+            new Tuple<long, long>(0x00CA5AB8, 0x00CA5AC8),
+            new Tuple<long, long>(0x00CA5B38, 0x00CA5B50),
             new Tuple<long, long>(0x00CA6640, 0x00CA9F90),
             new Tuple<long, long>(0x00D56050, 0x00D560C0),
             new Tuple<long, long>(0x00D99F90, 0x00D99F90),
